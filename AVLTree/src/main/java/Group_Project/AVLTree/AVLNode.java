5090f4e4621e2f98c2bf0b5dacd0b7982167296a
renstus <rsinjari@hotmail.com>
2015-03-24 13:16:33 -0400

    Implemented the AVLNode class.

--- AVLTree/src/main/java/Group_Project/AVLTree/AVLNode.java
+++ AVLTree/src/main/java/Group_Project/AVLTree/AVLNode.java
@@ -1,5 +1,27 @@
 package Group_Project.AVLTree;
 
+/*
+ * AVLNode class
+ */
+
 public class AVLNode {
 
+	AVLNode left;
+	AVLNode right;
+	AVLNode parent;
+	int data;
+	int height;
+	
+	public AVLNode(){
+		
+	}
+	public AVLNode(int data){
+		this.data = data;
+	}
+	public AVLNode(AVLNode left, AVLNode right, AVLNode parent ,int data){
+		this.left=left;
+		this.right = right;
+		this.parent = parent;
+		this.data = data;
+	}
 }
