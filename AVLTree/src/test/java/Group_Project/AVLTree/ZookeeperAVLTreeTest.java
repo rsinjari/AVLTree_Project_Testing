1f2ca2c3d586de32dc023308b9dbca2e767762f8
renstus <rsinjari@hotmail.com>
2015-03-26 16:00:37 -0400

    Changed the testing classes to all fail changes made to test Jenkins setup and jCoCo

--- AVLTree/src/test/java/Group_Project/AVLTree/ZookeeperAVLTreeTest.java
+++ AVLTree/src/test/java/Group_Project/AVLTree/ZookeeperAVLTreeTest.java
@@ -5,30 +5,30 @@ import org.junit.Test;
 public class ZookeeperAVLTreeTest {
 	@Test
 	public void testInsertNode(){
-		//throw new RuntimeException("test method not impleented");
+		throw new RuntimeException("test method not impleented");
 	}
 	@Test
 	public void testFindHight(){
-		//throw new RuntimeException("test method not impleented");
+		throw new RuntimeException("test method not impleented");
 	}
 	@Test
 	public void testDeleteNode(){
-		//throw new RuntimeException("test method not impleented");
+		throw new RuntimeException("test method not impleented");
 	}
 	@Test
 	public void testSearchValue(){
-		//throw new RuntimeException("test method not impleented");
+		throw new RuntimeException("test method not impleented");
 	}
 	@Test
 	public void testBalanceTree(){
-		//throw new RuntimeException("test method not impleented");
+		throw new RuntimeException("test method not impleented");
 	}
 	@Test
 	public void testCheckBalance(){
-		//throw new RuntimeException("test method not impleented");
+		throw new RuntimeException("test method not impleented");
 	}
 	@Test
 	public void testIsEmpty(){
-		//throw new RuntimeException("test method not impleented");
+		throw new RuntimeException("test method not impleented");
 	}
 }
