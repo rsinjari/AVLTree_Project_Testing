c458451d56be6306a13c3bcabe45a1d89061e15e
renstus <rsinjari@hotmail.com>
2015-03-26 17:46:12 -0400

    Implemented 5 out of the 7 unit testes for the AVLTree.java class

--- AVLTree/src/test/java/Group_Project/AVLTree/AVLTreeTest.java
+++ AVLTree/src/test/java/Group_Project/AVLTree/AVLTreeTest.java
@@ -14,11 +14,29 @@ public class AVLTreeTest {
 	
 	@Test
 	public void testInsertNode(){
+		assertNull("The tree is empty", t.root);
+		//asserts that it inserts on root
 		t.insertNode(1);
+		assertNotNull("The tree is not empty anymore",t.root);
+		assertEquals("The value entered in the tree is the one specified",1,t.root.data);
+		//assert it inserts a variable in the right place
+		t.insertNode(6);
+		assertEquals("Assert it inserts to the right of root", 6, t.root.right.data);
+		//asserts it inserts a variable to the left
+		t.insertNode(0);
+		assertEquals("Assert it inserts smaller value than the root to the left", 0,t.root.left.data);
+		//asserts it inserts two variables correctly and it balances the tree correctly
+		t.insertNode(4);
+		t.insertNode(5);
+		assertEquals("Assets position of 4",4,t.root.right.left.data);
+		assertEquals("Assets position of 6",6,t.root.right.right.data);
+		assertEquals("Assets position of 5",5,t.root.right.data);
+		
 	}
 	@Test
 	public void testFindHight(){
 		throw new RuntimeException("test method not impleented");
+		
 	}
 	@Test
 	public void testDeleteNode(){
@@ -26,18 +44,55 @@ public class AVLTreeTest {
 	}
 	@Test
 	public void testSearchValue(){
-		throw new RuntimeException("test method not impleented");
+		t.insertNode(5);
+		t.insertNode(0);
+		t.insertNode(2);
+		t.insertNode(3);
+		t.insertNode(10);
+		assertNull("checking that the variable doesnt exists in the tree", t.searchValue(8));
+		t.insertNode(8);
+		assertEquals("Checking that the value is found after beeing inserted", 8 , t.searchValue(8).data);
 	}
 	@Test
 	public void testBalanceTree(){
-		throw new RuntimeException("test method not impleented");
+		//The purpose of this method is to create a situation that requires the tree to self balance upon insertion
+		t.insertNode(25);
+		t.insertNode(57);
+		t.insertNode(17);
+		t.insertNode(1);
+		t.insertNode(19);
+		assertEquals("Asserts that 25 is the root", 25 , t.root.data);
+		assertEquals("Asserts that 17 is the left child", 17 , t.root.left.data);
+		assertEquals("Asserts that 57 is the right child", 57 , t.root.right.data);
+		assertEquals("Asserts that 1 is the left childs left child", 1 , t.root.left.left.data);
+		assertEquals("Asserts that 19 is the left childs right child", 19 , t.root.left.right.data);
+		
+		//we insert a value that breaks the balance and check if the tree selfbalances correctly
+		t.insertNode(10);
+		assertEquals("Asserts that 17 is the root", 17 , t.root.data);
+		assertEquals("Asserts that 1 is the left child", 1 , t.root.left.data);
+		assertEquals("Asserts that 25 is the right child", 25 , t.root.right.data);
+		assertEquals("Asserts that 19 is the right childs left child", 19 , t.root.right.left.data);
+		assertEquals("Asserts that 57 is the left childs right child", 57 , t.root.right.right.data);
+		assertEquals("Asserts that 10 is the left child right child", 10 , t.root.left.right.data);
 	}
 	@Test
 	public void testCheckBalance(){
-		throw new RuntimeException("test method not impleented");
+		t.insertNode(1);
+		assertEquals("Testing the hight of root", 1, t.findHeight(t.root));
+		t.insertNode(6);
+		t.insertNode(0);
+		t.insertNode(4);
+		t.insertNode(5);
+		assertEquals("Testing the hight of 6", 0, t.getDifference(t.root.right.right));
+		assertEquals("Testing the hight of 0", 0, t.getDifference(t.root.left));
+		assertEquals("Testing the hight of 4", 0, t.getDifference(t.root.right.left));
+		assertEquals("Testing the hight of 5", 0, t.getDifference(t.root.right));
 	}
 	@Test
 	public void testIsEmpty(){
-		throw new RuntimeException("test method not impleented");
+		assertTrue("Checking that the tree is empty and returns null.",t.isEmpty());
+		t.insertNode(10);
+		assertFalse("Checking that there is a root and its not null",t.isEmpty());
 	}
 }
